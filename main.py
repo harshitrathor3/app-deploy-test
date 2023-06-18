28dd8387bb59684f581449b4132d9e3e81ad7269
Harshit Rathore <105155118+harshitrathor3@users.noreply.github.com>
2023-06-18 20:31:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+
+
+name = st.text_input('Enter you name')
+st.write(name)
+num = st.text_input('Enter a number')
+if num:
+    st.write(np.exp(int(num)))
